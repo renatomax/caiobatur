b2a977c8f601ef4ad2f633b8990ddd4fab057164
renatomax <renato.maximiano.junior@gmail.com>
2023-09-18 11:11:56 -0300

    first commit

---
+++ scripts/animandoComent.js
@@ -0,0 +1,35 @@
+const comentarios = document.querySelectorAll(".cometarioBox");
+
+let indexComentarioAnimado = 0;
+
+function comentarioAnimado() {
+  comentarios[indexComentarioAnimado].style.animation =
+    "sobeComentario 15s linear";
+  comentarios[indexComentarioAnimado].style.left = randomBetween(0, 80) + "px";
+  chamandoProxComent();
+}
+
+function chamandoProxComent() {
+  if (indexComentarioAnimado == comentarios.length - 1) {
+    indexComentarioAnimado = 0;
+  } else {
+    indexComentarioAnimado++;
+  }
+
+  setTimeout(() => {
+    comentarioAnimado();
+  }, randomBetween(5000, 7000));
+}
+
+window.addEventListener("animationend", (event) => {
+  if (event.animationName == "sobeComentario") {
+    let elemento = event.target;
+    elemento.style.animation = "";
+  }
+});
+
+comentarioAnimado();
+
+function randomBetween(min, max) {
+  return Math.random() * (max - min) + min;
+}
